436a367688bc2942f6435dbfe40431ba96853880
fcoperezgil <franciscovalde@hotmail.com>
2016-02-19 17:15:08 +0100

    Improve the style and the behaviour of the main page

--- public-server/public/js/main.js
+++ public-server/public/js/main.js
@@ -1,8 +1,9 @@
+/*
 $('.nav li').click(function(e) {
   var $this = $(this);
   if (!$this.hasClass('active')) {
     $this.addClass('active');
   }
   $('.nav li').not($this).removeClass("active");
-  e.preventDefault();
 });
+*/
