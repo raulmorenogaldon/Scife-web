f14b427943f8795c45e8a934a29b3fdf978765ea
fcoperezgil <franciscovalde@hotmail.com>
2016-02-19 15:11:16 +0100

    Add interface for API REST users

--- public-server/routes/users.js
+++ public-server/routes/users.js
@@ -1,73 +1,95 @@
 var express = require('express'),
-    router = express.Router(),
-    mongoose = require('mongoose'),
-    User = mongoose.model('User');
+  router = express.Router(),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User')
 
-
-router.get('/index', function(req, res) {
+/**
+ * Return a list with the users saved in the database
+ */
+router.get('/', function (req, res) {
   User.find(function (err, users) {
-      if(err)res.send(500, err.message);
-      
-      console.log(users);
-      res.render('user/index', {users:users});
-  });
-});
+    if (err)res.send(500, err.message)
+    res.render('users/index', {users: users})
+  })
+})
 
-router.get('/find/:id', function(req, res) {
+/**
+ * Return the user's info whose id is passed as a parameter
+ */
+router.get('/info/:id', function (req, res) {
   User.findById(req.params.id, function (err, user) {
-    if(err)res.send(500, err.message);
-    res.setHeader("Content-type", "application/json");
-    res.status(200); 
-    res.send(JSON.stringify(user));
-  });
-});
+    if (err)res.send(500, err.message)
+    res.render('users/info', {user: user})
+  })
+})
 
-router.post('/create', function(req, res) {
-    console.log('POST /user/create');
-        console.log(req.body);
-        
-        var user = new User({
-            userName: req.body.userName,
-            email: req.body.email,
-            password: req.body.password
-        });
-        
-        user.save(function(err, user){
-           if(err) return res.status(500).send(err.message);
-           res.setHeader("Content-type", "application/json");
-           res.status(200); 
-           res.send(JSON.stringify(user));
-        });
-});
+/**
+ * Get the form to add a new user
+ */
+router.get('/create', function (req, res) {
+  res.render('users/create')
+})
 
-router.post('/update/:id', function(req, res) {
-    
-        console.log(req.body);
-    User.findById(req.params.id, function (err, user) {
-        if(err) return res.send(500, err.message);
-        
-        user.userName = req.body.userName;
-        user.email = req.body.email;
-        user.password = req.body.password;
-        
-        user.save(function (err) {
-            if(err) return res.send(500, err.message);
-            res.setHeader("Content-type", "application/json");
-            res.status(200); 
-            res.send(JSON.stringify(user));
-        });
-    });
-});
+/**
+ * Receive the info to create a new user
+ */
+router.post('/create', function (req, res) {
+  var user = new User({
+    userName: req.body.userName,
+    email: req.body.email,
+    password: req.body.password
+  })
 
-router.get('/delete/:id', function(req, res) {
+  user.save(function (err, user) {
+    if (err) return res.status(500).send(err.message)
+    res.redirect('/users/')
+  })
+})
+
+/**
+ * Get the form to update the user's info
+ */
+router.get('/update/:id', function (req, res) {
   User.findById(req.params.id, function (err, user) {
-        if(err) return res.send(500, err.message);
-        
-        user.remove(function (err) {
-            if(err) return res.send(500, err.message);
-            return res.status(200).send('User Deleted');
-        });
-    });
-});
+    if (err) return res.send(500, err.message)
+    res.render('users/update', {user: user})
+  })
+})
 
-module.exports = router;
+router.post('/update/:id', function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    if (err) return res.send(500, err.message)
+
+    user.userName = req.body.userName
+    user.email = req.body.email
+    user.password = req.body.password
+
+    user.save(function (err) {
+      if (err) return res.send(500, err.message)
+      res.setHeader('Content-type', 'application/json')
+      res.status(200)
+      res.send(JSON.stringify(user))
+    })
+  })
+})
+
+router.get('/delete/:id', function (req, res) {
+  User.findById(req.params.id, function (err, user) {
+    if (err) return res.send(500, err.message)
+
+    res.render('users/delete', {user: user})
+  })
+})
+
+router.post('/delete/', function (req, res) {
+  User.findById(req.body.id, function (err, user) {
+    if (err) return res.send(500, err.message)
+
+    user.remove(function (err) {
+      if (err)res.send(500, err.message)
+      res.redirect('/users/')
+    })
+  })
+})
+
+module.exports = router
