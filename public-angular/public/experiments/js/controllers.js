998a307191459f8c8fbd524c7c3c623bd5bfb953
fcoperezgil <franciscovalde@hotmail.com>
2016-05-23 09:32:01 +0200

    Implemented the progress bar modal to show when the app is doing something

--- public-angular/public/experiments/js/controllers.js
+++ public-angular/public/experiments/js/controllers.js
@@ -1,256 +1,263 @@
 var app = angular.module('Experiments', ['ui.router', 'angularTreeview'])
 
-	.controller('SidebarCtrl', ['$scope', '$location', '$stateParams', function ($scope, $location, $stateParams) {
-		$scope.links = [{
-			name: "Overview",
-			url: "/overview/" + $stateParams.experimentId
-		}, {
-				name: "Labels",
-				url: "/labels/" + $stateParams.experimentId
-			}, {
-				name: "Input Data",
-				url: "/inputdata/" + $stateParams.experimentId
-			}, {
-				name: "Sources",
-				url: "/sources/" + $stateParams.experimentId
-			}, {
-				name: "Logs",
-				url: "/logs/" + $stateParams.experimentId
-			}];
+.controller('SidebarCtrl', ['$scope', '$location', '$stateParams', function($scope, $location, $stateParams) {
+	$scope.links = [{
+		name: "Overview",
+		url: "/overview/" + $stateParams.experimentId
+	}, {
+		name: "Labels",
+		url: "/labels/" + $stateParams.experimentId
+	}, {
+		name: "Input Data",
+		url: "/inputdata/" + $stateParams.experimentId
+	}, {
+		name: "Sources",
+		url: "/sources/" + $stateParams.experimentId
+	}, {
+		name: "Logs",
+		url: "/logs/" + $stateParams.experimentId
+	}];
 
-		$scope.isActive = function (route) {
-			return route === $location.path();
-		};
+	$scope.isActive = function(route) {
+		return route === $location.path();
+	};
 
-	}])
+}])
 
-	.controller('IndexCtrl', ['$scope', '$http', 'ExpDataService', 'BgColors',
-		function ($scope, $http, ExpDataService, BgColors) {
-			function getList() {
-				$http.get('/experiments/list/')
-					.then(function (response) {
-						$scope.experiments = response.data;
-						$http.get('/applications/list/')
-							.then(function (data) {
-								$scope.experiments.forEach(function (exp) {
-									exp.app = angular.copy(data.data.find(function (app) {
-										return app.id === exp.app_id;
-									}));
-								});
-							}, function (response) {
-								$scope.errors = response.data.errors;
+.controller('IndexCtrl', ['$scope', '$http', 'ExpDataService', 'BgColors',
+	function($scope, $http, ExpDataService, BgColors) {
+		function getList() {
+			$http.get('/experiments/list/')
+				.then(function(response) {
+					$scope.experiments = response.data;
+					$http.get('/applications/list/')
+						.then(function(data) {
+							$scope.experiments.forEach(function(exp) {
+								exp.app = angular.copy(data.data.find(function(app) {
+									return app.id === exp.app_id;
+								}));
 							});
-					}, function (response) {
-						$scope.errors = response.data.errors;
-					});
-			}
-			getList();
-
-			$scope.saveExp = function (exp) {
-				ExpDataService.set(exp);
-			};
-
-			$scope.selectExpToDelete = function (exp) {
-				$scope.deleteExpSelect = exp;
-			};
-
-			$scope.deleteSubmit = function () {
-				$http.delete('/experiments/' + $scope.deleteExpSelect.id)
-					.then(function (response) {
-						getList();
-						$scope.message = "Experiment " + $scope.deleteExpSelect.name + " delete successfuly.";
-						ExpDataService.set(null);
-					}, function (response) {
-						$scope.errors = "There is an error in the request";
-					});
-				jQuery('#deleteModal').modal('hide');
-			};
-
-			$scope.bgColors = BgColors;
-		}
-	])
-
-	.controller('OverviewCtrl', ['$scope', '$http', '$stateParams', 'ExpDataService', '$interval', '$location', 'BgColors', function ($scope, $http, $stateParams, ExpDataService, $interval, $location, BgColors) {
-		var timer;
-		$scope.experiment = ExpDataService.get();
-		if (!$scope.experiment) {
-			$http.get('/experiments/details/' + $stateParams.experimentId)
-				.then(function (response) {
-					$scope.experiment = response.data;
-					$http.get('/applications/details/' + $scope.experiment.app_id)
-						.then(function (data) {
-							$scope.experiment.app = data.data;
-						}, function (data) {
-							$scope.errors = data.data.errors;
+						}, function(response) {
+							$scope.errors = response.data.errors;
 						});
-				}, function (response) {
+				}, function(response) {
 					$scope.errors = response.data.errors;
-					console.log($scope.errors);
 				});
 		}
+		getList();
 
-
-		$scope.refreshStatus = function () {
-			$http.get('/experiments/details/' + $stateParams.experimentId)
-				.then(function (response) {
-					$scope.experiment.status = response.data.status;
-				}, function (response) {
-					$scope.errors = response.data.errors;
-				});
+		$scope.saveExp = function(exp) {
+			ExpDataService.set(exp);
 		};
 
-		$scope.launchModal = function () {
-			$scope.launchData = {};
-			$http.get('/images/list')
-				.then(function (response) {
-					$scope.images = response.data;
-					$scope.launchData.image_id = response.data[0].id;
-				}, function (response) {
-					$scope.errors = response.data.errors;
-				});
-			$http.get('/sizes/list')
-				.then(function (response) {
-					$scope.sizes = response.data;
-					$scope.launchData.size_id = response.data[0].id;
-				}, function (response) {
-					$scope.errors = response.data.errors;
-				});
+		$scope.selectExpToDelete = function(exp) {
+			$scope.deleteExpSelect = exp;
 		};
 
-		$scope.launchSubmit = function () {
-			$http.post('/experiments/launch/' + $scope.experiment.id, $scope.launchData)
-				.then(function (response) {
-					$scope.message = response.data.message;
-					$scope.refreshStatus();
-				}, function (response) {
-					$scope.errors = response.data.errors;
-				});
-			jQuery('#launchModal').modal('hide');
-			timer = $interval($scope.refreshStatus, 10000);
-		};
-
-		$scope.reset = function () {
-			$http.post('/experiments/reset/' + $scope.experiment.id, $scope.launchData)
-				.then(function (response) {
-					$scope.message = response.data.message;
-					$scope.refreshStatus();
-				}, function (response) {
-					$scope.errors = response.data.errors;
-				});
-			$interval.cancel(timer);
-		};
-
-		$scope.deleteSubmit = function () {
-			$http.delete('/experiments/' + $scope.experiment.id)
-				.then(function (response) {
+		$scope.deleteSubmit = function() {
+			jQuery('#loadingModal').modal('show');
+			$http.delete('/experiments/' + $scope.deleteExpSelect.id)
+				.then(function(response) {
+					getList();
+					$scope.message = "Experiment " + $scope.deleteExpSelect.name + " delete successfuly.";
 					ExpDataService.set(null);
-					jQuery('#deleteModal').modal('hide');
-					$interval(function () {
-						$location.path('/');
-					}, 500, 1, true);
-				},
-					function (response) {
-						$scope.errors = "There is an error in the request";
-					});
-			$interval.cancel(timer);
-		};
-
-		$scope.downloadResults = function (id) {
-			window.open('/experiments/downloadresults/' + id);
+					jQuery('#loadingModal').modal('hide');
+				}, function(response) {
+					$scope.errors = "There is an error in the request";
+				});
+			jQuery('#deleteModal').modal('hide');
 		};
 
 		$scope.bgColors = BgColors;
-	}])
+	}
+])
 
-	.controller('LabelsCtrl', ['$scope', '$http', '$stateParams', 'ExpDataService', function ($scope, $http, $stateParams, ExpDataService) {
-		$scope.experiment = ExpDataService.get(); // Get the experiment from the service.
-		$scope.showForm = false;
+.controller('OverviewCtrl', ['$scope', '$http', '$stateParams', 'ExpDataService', '$interval', '$location', 'BgColors', function($scope, $http, $stateParams, ExpDataService, $interval, $location, BgColors) {
+	var timer;
+	$scope.experiment = ExpDataService.get();
+	if (!$scope.experiment) {
+		$http.get('/experiments/details/' + $stateParams.experimentId)
+			.then(function(response) {
+				$scope.experiment = response.data;
+				$http.get('/applications/details/' + $scope.experiment.app_id)
+					.then(function(data) {
+						$scope.experiment.app = data.data;
+					}, function(data) {
+						$scope.errors = data.data.errors;
+					});
+			}, function(response) {
+				$scope.errors = response.data.errors;
+				console.log($scope.errors);
+			});
+	}
 
-		if (!$scope.experiment) {
-			$http.get('/experiments/details/' + $stateParams.experimentId)
-				.then(function (response) {
-					$scope.experiment = response.data;
-					$scope.oldLabels = angular.copy(response.data.labels);
-					$http.get('/applications/details/' + $scope.experiment.app_id)
-						.then(function (data) {
-							$scope.experiment.app = data.data;
-						}, function (data) {
-							$scope.errors = response.data.errors;
-						});
-				}, function (response) {
-					$scope.errors = response.data.errors;
+
+	$scope.refreshStatus = function() {
+		$http.get('/experiments/details/' + $stateParams.experimentId)
+			.then(function(response) {
+				$scope.experiment.status = response.data.status;
+			}, function(response) {
+				$scope.errors = response.data.errors;
+			});
+	};
+
+	$scope.launchModal = function() {
+		$scope.launchData = {};
+		$http.get('/images/list')
+			.then(function(response) {
+				$scope.images = response.data;
+				$scope.launchData.image_id = response.data[0].id;
+			}, function(response) {
+				$scope.errors = response.data.errors;
+			});
+		$http.get('/sizes/list')
+			.then(function(response) {
+				$scope.sizes = response.data;
+				$scope.launchData.size_id = response.data[0].id;
+			}, function(response) {
+				$scope.errors = response.data.errors;
+			});
+	};
+
+	$scope.launchSubmit = function() {
+		$http.post('/experiments/launch/' + $scope.experiment.id, $scope.launchData)
+			.then(function(response) {
+				$scope.message = response.data.message;
+				$scope.refreshStatus();
+			}, function(response) {
+				$scope.errors = response.data.errors;
+			});
+		jQuery('#launchModal').modal('hide');
+		timer = $interval($scope.refreshStatus, 10000);
+	};
+
+	$scope.reset = function() {
+		$http.post('/experiments/reset/' + $scope.experiment.id, $scope.launchData)
+			.then(function(response) {
+				$scope.message = response.data.message;
+				$scope.refreshStatus();
+			}, function(response) {
+				$scope.errors = response.data.errors;
+			});
+		$interval.cancel(timer);
+	};
+
+	$scope.deleteSubmit = function() {
+		$http.delete('/experiments/' + $scope.experiment.id)
+			.then(function(response) {
+					ExpDataService.set(null);
+					jQuery('#deleteModal').modal('hide');
+					$interval(function() {
+						$location.path('/');
+					}, 500, 1, true);
+				},
+				function(response) {
+					$scope.errors = "There is an error in the request";
 				});
-		} else {
-			$scope.oldLabels = angular.copy($scope.experiment.labels);
-		}
+		$interval.cancel(timer);
+	};
 
-		$scope.submit = function () {
-			for (var i in $scope.experiment.labels) {
-				if ($scope.experiment.labels[i] === "" || $scope.experiment.labels[i] === null) {
-					delete $scope.experiment.labels[i];
-				}
+	$scope.downloadResults = function(id) {
+		window.open('/experiments/downloadresults/' + id);
+	};
+
+	$scope.bgColors = BgColors;
+}])
+
+.controller('LabelsCtrl', ['$scope', '$http', '$stateParams', 'ExpDataService', function($scope, $http, $stateParams, ExpDataService) {
+	$scope.experiment = ExpDataService.get(); // Get the experiment from the service.
+	$scope.showForm = false;
+
+	if (!$scope.experiment) {
+		$http.get('/experiments/details/' + $stateParams.experimentId)
+			.then(function(response) {
+				$scope.experiment = response.data;
+				$scope.oldLabels = angular.copy(response.data.labels);
+				$http.get('/applications/details/' + $scope.experiment.app_id)
+					.then(function(data) {
+						$scope.experiment.app = data.data;
+					}, function(data) {
+						$scope.errors = response.data.errors;
+					});
+			}, function(response) {
+				$scope.errors = response.data.errors;
+			});
+	} else {
+		$scope.oldLabels = angular.copy($scope.experiment.labels);
+	}
+
+	$scope.submit = function() {
+		for (var i in $scope.experiment.labels) {
+			if ($scope.experiment.labels[i] === "" || $scope.experiment.labels[i] === null) {
+				delete $scope.experiment.labels[i];
 			}
-			$http.put('/experiments/' + $scope.experiment.id, {
-				labels: $scope.experiment.labels
-			}).then(function (response) {
-				$scope.message = "Experiment " + $scope.experiment.name + " updated successfully.";
-				$scope.showForm = false;
-				$scope.oldLabels = angular.copy($scope.experiment.labels);
-			}, function (response) {
-				console.log(response);
-				$scope.errors = "There is an error in the request.";
-			});
-		};
-
-		$scope.cancel = function () {
+		}
+		$http.put('/experiments/' + $scope.experiment.id, {
+			labels: $scope.experiment.labels
+		}).then(function(response) {
+			$scope.message = "Experiment " + $scope.experiment.name + " updated successfully.";
 			$scope.showForm = false;
-			$scope.experiment.labels = $scope.oldLabels;
-		};
-	}])
+			$scope.oldLabels = angular.copy($scope.experiment.labels);
+		}, function(response) {
+			console.log(response);
+			$scope.errors = "There is an error in the request.";
+		});
+	};
 
-	.controller('CreateCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-		$scope.experiment = {};
-		$http.get('/applications/list/')
-			.then(function (response) {
-				$scope.applications = response.data;
-				$scope.experiment.app_id = response.data[0].id;
-			}, function (response) {
-				$scope.errors = response.data.errors;
-			});
+	$scope.cancel = function() {
+		$scope.showForm = false;
+		$scope.experiment.labels = $scope.oldLabels;
+	};
+}])
 
-		$scope.submit = function () {
-			$http.post('/experiments/create', $scope.experiment)
-				.then(function (response) {
+.controller('CreateCtrl', ['$scope', '$http', '$location', '$interval', function($scope, $http, $location, $interval) {
+	$scope.experiment = {};
+	$http.get('/applications/list/')
+		.then(function(response) {
+			$scope.applications = response.data;
+			$scope.experiment.app_id = response.data[0].id;
+		}, function(response) {
+			$scope.errors = response.data.errors;
+		});
+
+	$scope.submit = function() {
+		jQuery('#loadingModal').modal('show');
+		$http.post('/experiments/create', $scope.experiment)
+			.then(function(response) {
+				console.log("Acaba");
+				jQuery('#loadingModal').modal('hide');
+				$interval(function() {
 					$location.path('overview/' + response.data.experimentId);
-				}, function (response) {
-					$scope.errors = response.data.errors;
-				});
-		};
-	}])
-
-	.controller('InputDataCtrl', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
-		$http.get('/experiments/details/' + $stateParams.experimentId)
-			.then(function (response) {
-				$scope.experiment = response.data;
-			}, function (response) {
+				}, 200, 1, true);
+			}, function(response) {
 				$scope.errors = response.data.errors;
 			});
-	}])
+	};
+}])
 
-	.controller('SourcesCtrl', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
-		$http.get('/experiments/details/' + $stateParams.experimentId)
-			.then(function (response) {
-				$scope.experiment = response.data;
-			}, function (response) {
-				$scope.errors = response.data.errors;
-			});
-	}])
+.controller('InputDataCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+	$http.get('/experiments/details/' + $stateParams.experimentId)
+		.then(function(response) {
+			$scope.experiment = response.data;
+		}, function(response) {
+			$scope.errors = response.data.errors;
+		});
+}])
 
-	.controller('LogsCtrl', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
-		$http.get('/experiments/logs/' + $stateParams.experimentId)
-			.then(function (response) {
-				$scope.logs = response.data;
-			}, function (response) {
-				$scope.errors = response.data.errors;
-			});
-	}]);
+.controller('SourcesCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+	$http.get('/experiments/details/' + $stateParams.experimentId)
+		.then(function(response) {
+			$scope.experiment = response.data;
+		}, function(response) {
+			$scope.errors = response.data.errors;
+		});
+}])
+
+.controller('LogsCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+	$http.get('/experiments/logs/' + $stateParams.experimentId)
+		.then(function(response) {
+			$scope.logs = response.data;
+		}, function(response) {
+			$scope.errors = response.data.errors;
+		});
+}]);
