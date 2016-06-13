0603e7f519e351fe07997a5074e2c99a2d70726f
Oneadmin <oneadmin@oneadmin.com>
2016-06-13 16:04:27 +0200

    [PUBLIC] Changed MultiCloud strings to Scife.

--- public-angular/config/env/development.js
+++ public-angular/config/env/development.js
@@ -1,6 +1,5 @@
 module.exports = {
 	db: 'mongodb://localhost/multicloud',
 	sessionSecret: 'SesionSecretForMultiCloud',
-	//privateServer: 'http://161.67.100.107:4000',
 	privateServer: 'http://scife.ddns.net:4000'
 };
