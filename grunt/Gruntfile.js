15fb53b278db6b6e8fd9e56a637542d330b44465
fcoperezgil <franciscovalde6@gmail.com>
2016-07-07 13:28:28 +0200

    Add analyze for css in grunt and fix some bugs detected for grunt

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
    grunt.initConfig({
       jshint: {
          files: ['../public-angular/app/**/*.js', '../public-angular/config/**/*.js', '../public-angular/public/experiments/**/*.js'],
@@ -14,20 +14,33 @@ module.exports = function(grunt) {
       htmlangular: {
          options: {
             angular: true,
-            charset: "utf-8",
-            relaxerror: ['Start tag seen without seeing a doctype first. Expected e.g. “<!DOCTYPE html>”.', 'Element “head” is missing a required instance of child element “title”.',
-               'A “select” element with a “required” attribute, and without a “multiple” attribute,',
-               'The first child “option” element of a “select” element with a “required” attribute,',
-               'Bad value “'
-            ]
+				customtags: ['file-model'],
+            relaxerror: ['Start tag seen without seeing a doctype first. Expected e.g. "<!DOCTYPE html>".',
+					"Element 'head' is missing a required instance of child element 'title'.",
+
+               'A "select" element with a "required" attribute, and without a "multiple" attribute,',
+
+               'The first child "option" element of a "select" element with a "required" attribute,',
+
+               'Bad value "'
+            ],
+				reportPath:'html-angular-log.json'
          },
          files: ['../public-angular/public/experiments/views/*.html']
-
-      }
+      },
+		csslint: {
+			strict: {
+				options: {
+					import: 2
+				},
+				src: ['../public-angular/public/css/**/*.css','../public-angular/public/experiment/css/**/*.css']
+			},
+		}
    });
 
-   grunt.loadNpmTasks('grunt-contrib-jshint');
-   grunt.loadNpmTasks('grunt-html-angular-validate');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-html-angular-validate');
+	grunt.loadNpmTasks('grunt-contrib-csslint');
 
-   grunt.registerTask('default', ['jshint', 'htmlangular']);
+	grunt.registerTask('default', ['jshint', /*'htmlangular',*/ 'csslint']);
 };
