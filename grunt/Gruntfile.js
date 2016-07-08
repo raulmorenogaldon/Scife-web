f13d7788182e8f924455447244136f6d684faeb4
fcoperezgil <franciscovalde6@gmail.com>
2016-07-08 10:49:36 +0200

    Add the uglify and concat functions to grunt project

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function (grunt) {
 
                'Bad value "'
             ],
-				reportPath:'html-angular-log.json'
+				reportPath: 'html-angular-log.json'
          },
          files: ['../public-angular/public/experiments/views/*.html']
       },
@@ -33,14 +33,32 @@ module.exports = function (grunt) {
 				options: {
 					import: 2
 				},
-				src: ['../public-angular/public/css/**/*.css','../public-angular/public/experiment/css/**/*.css']
+				src: ['../public-angular/public/css/**/*.css', '../public-angular/public/experiment/css/**/*.css']
 			},
+		},
+		concat: {
+			dist: {
+				src: ['../public-angular/public/experiments/js/*.js'],
+				dest: ['../public-angular/public/experiments/js/experiments-concat.js']
+			}
+		},
+		uglify: {
+			my_target: {
+				options: {
+					compress: true
+				},
+				files: {
+					'../public-angular/public/experiments/js/experiments-concat.min.js': ['../public-angular/public/experiments/js/*.js']
+				}
+			}
 		}
    });
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-html-angular-validate');
 	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-	grunt.registerTask('default', ['jshint', /*'htmlangular',*/ 'csslint']);
+	grunt.registerTask('default', ['jshint', /*'htmlangular',*/ 'csslint','concat','uglify']);
 };
